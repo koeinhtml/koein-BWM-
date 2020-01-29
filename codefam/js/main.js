416ceaad6102ee355c93bbeccac628cf6516abd6
mainwebdev <realguydev@gmail.com>
2020-01-29 16:20:56 +0200

    yurii 29.01.20

--- codefam/js/main.js
+++ codefam/js/main.js
@@ -6,33 +6,71 @@ $(document).ready(function() {
 
 function slidersInit() {
 
+	if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
+
 	if ($('.js-latest-news-slider').length) {
 		var mySwiper = new Swiper ('.js-latest-news-slider', {
-			slidesPerView: 3,
+			slidesPerView: 1,
+			spaceBetween: 30,
 			keyboard: {
 		    enabled: true,
 		  },
 	    pagination: {
 	      el: '.swiper-pagination',
 	    },
+	    breakpoints: {
+
+		    576: {
+		      slidesPerView: 1,
+		    },
+
+		    767: {
+		      slidesPerView: 2,
+		    },
+
+		    991: {
+		      slidesPerView: 3,
+		    }
+		  }
 	  })
 	}
 
 	if ($('.js-we-work-slider').length) {
 		var mySwiper = new Swiper ('.js-we-work-slider', {
-			slidesPerView: 3,
+			slidesPerView: 1,
 			keyboard: {
 		    enabled: true,
 		  },
 	    pagination: {
 	      el: '.swiper-pagination',
 	    },
+	    breakpoints: {
+
+		    576: {
+		      slidesPerView: 3,
+		    },
+
+		    767: {
+		      slidesPerView: 2,
+		    },
+
+		    991: {
+		      slidesPerView: 3,
+		    }
+		  }
 	  })
 	}
 }
 
 function selectInit() {
-	$('.js-example-basic-single').select2();
+	$('.js-example-basic-single').select2({
+		width: 'resolve',
+	});
 }
 
 function mobMenu() {
