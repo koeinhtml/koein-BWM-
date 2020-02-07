4e806d7c29302266f944d756369a68230e594900
mainwebdev <realguydev@gmail.com>
2020-02-07 21:02:58 +0200

    yurii. 07.02.20

--- codefam/js/tables.js
+++ codefam/js/tables.js
@@ -10,9 +10,70 @@ function tableInit() {
 		sortable: {
 			mode: "multiple",
 			allowUnsort: true,
-			showIndexes: true
 		},
 		columnMenu: true,
-		
 	});
+
+	$(".js-project-suplier-table").kendoGrid({
+		sortable: true,
+		filterable: true,
+		scrollable: false,
+		sortable: {
+			mode: "multiple",
+			allowUnsort: true,
+		},
+		columnMenu: true,
+		detailInit: detailInit,
+	});
+
+	function detailInit(e) {
+		$("<div/>").appendTo(e.detailCell).kendoGrid({
+			dataSource: {
+        data: [{
+            ProjectTaskName: "Task 1",
+            ProjectManager: "Joyce Attoue",
+            TaskType: "Piping",
+            PurchaseOrder: "35122",
+            MoneyIssued: "200 000 $",
+            PendingAmount: "100 000 $",
+            PaidAmount: "100 000 $",
+            Status: "100 000 $",
+        },
+        {
+            ProjectTaskName: "Task 2",
+            ProjectManager: "Joyce Attoue",
+            TaskType: "Piping",
+            PurchaseOrder: "35122",
+            MoneyIssued: "200 000 $",
+            PendingAmount: "100 000 $",
+            PaidAmount: "100 000 $",
+            Status: "100 000 $",
+        },
+        {
+            ProjectTaskName: "Task 3",
+            ProjectManager: "Joyce Attoue",
+            TaskType: "Piping",
+            PurchaseOrder: "35122",
+            MoneyIssued: "200 000 $",
+            PendingAmount: "100 000 $",
+            PaidAmount: "100 000 $",
+            Status: "100 000 $",
+        },
+        ]
+      },
+      
+			scrollable: false,
+			sortable: true,
+			columns: [
+			{ field: "ProjectTaskName", title:"Project/Task Name" },
+			{ field: "ProjectManager", title:"Project Manager" },
+			{ field: "TaskType", title:"Task Type" },
+			{ field: "PurchaseOrder", title: "Purchase Order" },
+			{ field: "MoneyIssued", title: "Money Issued" },
+			{ field: "PaidAmount", title: "Money Issued" },
+			{ field: "PendingAmount", title: "Pending Amount" },
+			{ field: "Status", title: "Status" },
+			]
+		});
+	}
 }
